ce303649e0740c2fdb3218203eabc9913a042b42
anjilinux <anjireddy2024@gmail.com>
2026-02-06 20:20:40 +0530

    data

--- test_data.py
+++ test_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def test_data_loading():
-    df = pd.read_csv("data/raw/spam.csv")
+    df = pd.read_csv("spam.csv")
     assert not df.empty
     assert "label" in df.columns
     assert "text" in df.columns
